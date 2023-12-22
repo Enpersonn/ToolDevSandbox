b0b9ae058f94fe0de589a66f58a1fa770490bd71
Enpersonn <nicklas.b.overjordet@gmail.com>
2023-12-22 16:33:36 +0100

    continued work on the rich text editor

--- src/app/(project)/wysiwyg/page.tsx
+++ src/app/(project)/wysiwyg/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 import TextEditorField from "@/components/textEditor_WYSIWYG/TextEditorField";
 import TextEditorHud from "@/components/textEditor_WYSIWYG/TextEditorHud";
-import { useRef, useEffect, Children } from "react";
+import { clsx } from "@/utils/classes";
+import { useRef, useEffect } from "react";
 
 
 export default function wysiwyg() {
     const fileButton = " font-bold text-xl bg-slate-900 text-white  rounded-t-xl v w-fit px-5 border-b-2 border-slate-800 select-none hover:bg-slate-500 hover:text-white transition-all duration-200"
+    const styelButton = "flex w-6 h-6 bg-slate-500 rounded-md items-center justify-center"
 
     const TranslateFrame = useRef<HTMLDivElement>(null)
 
@@ -25,42 +27,80 @@ export default function wysiwyg() {
 
         for (let i = 0; i < selection.rangeCount; i++) {
             const range = selection.getRangeAt(i);
-            const parent = getParent(range)
+            const parent1 = getParent(range)
+            const parent2 = range.commonAncestorContainer.parentElement
+
+            const parent = (parent1 != undefined ? parent1 as HTMLDivElement : parent2 as HTMLSpanElement)
+
+            console.log('Range:', range);
+            console.log('parent:', parent);
             if (!parent) return
+
+            findPartiallySelectedElements(parent)
+
+            if (!(style == "bold" || style == "italic")) return
 
             //if element naboers a span with same style fuse them
             const nSpan = document.createElement("span")
 
             //mby try to find a better way to diferantiate between style changes
-            if (style == "bold") nSpan.style.fontWeight = "bold"
-            if (style == "italic") nSpan.style.fontStyle = "italic"
+            if (style == "bold") MakeBold(range, nSpan, parent)
+            if (style == "italic") MakeItalic(range, nSpan, parent)
+
 
             //fix so if span is partialy inside another element split that element so the span can go over the text
             //fix so text is not scrwed up by inserting new element into it
-            range.surroundContents(nSpan)
-            if (nSpan.hasChildNodes()) {
-                const nSpanChildren = nSpan.children
-                for (let j = 0; j < nSpanChildren.length; j++) {
-                    const childElement = nSpanChildren[j]
-                    console.log(window.getComputedStyle(childElement).fontWeight)
-                    if (style == "bold" && window.getComputedStyle(childElement).fontWeight == "700") {
-                        console.log("godt this far")
-                        //fix so innerHtml of child is not deleted but taken out then remove the element
-                        nSpan.removeChild(childElement)
 
-                    }
-                }
-            }
-
-            console.log('Range:', range);
-            console.log('parent:', parent);
         }
 
     }
 
+    const MakeBold = (range: Range, elem: HTMLSpanElement, parent: Element) => {
+        console.log(parent.getAttributeNames)
+        elem.style.fontWeight = "bold"
+        range.surroundContents(elem)
+
+        const fragmentChildren = elem.children
+        for (let j = 0; j < fragmentChildren.length; j++) {
+            const child = fragmentChildren[j]
+            console.log(window.getComputedStyle(child).fontWeight)
+
+            if (window.getComputedStyle(child).fontWeight == "700" && child.firstChild) {
+                console.log("it was him your honor",)
+                elem.insertBefore(child.firstChild, child)
+                elem.removeChild(child)
+            }
+            const stringTxt = elem.innerText.toString()
+        }
+
+        console.log('Range:', range);
+
+    }
+
+    const MakeItalic = (range: Range, elem: HTMLSpanElement, parent: Node) => {
+        elem.style.fontStyle = "italic"
+        range.surroundContents(elem)
+
+
+    }
+
+    const findPartiallySelectedElements = (parent: Element) => {
+
+        const walker = document.createTreeWalker(parent, NodeFilter.SHOW_ELEMENT, null);
+
+        while (walker.nextNode()) {
+            const node = walker.currentNode;
+            const nodeRange = document.createRange();
+            nodeRange.selectNodeContents(node);
+            console.log(node)
+        }
+
+
+        return;
+    };
+
+
     const getParent = (range: Range) => {
-
-
         const commonAncestor = range.commonAncestorContainer
         if (commonAncestor == TranslateFrame.current) {
             return (commonAncestor)
@@ -85,17 +125,28 @@ export default function wysiwyg() {
                 <button className={fileButton}>TextEditor</button>
 
             </div>
-            <div className=" bg-slate-900 text-white w-full text-left shadow-xl rounded-xl p-1">
+            <div className=" bg-slate-900 text-white w-full text-left shadow-xl rounded-xl ">
                 <div className=" w-full h-full">
                     {/* <TextEditorHud />
                     <TextEditorField /> */}
-                    <div className=" flex gap-3">
-                        <button onClick={() => addFontStyle("bold")} className="flex w-6 h-6 bg-slate-500 rounded-md items-center justify-center font-bold">B</button>
-                        <button onClick={() => addFontStyle("italic")} className="flex w-6 h-6 bg-slate-500 rounded-md items-center justify-center italic">i</button>
+                    <div className=" px-5 py-2 flex gap-3 border-b-2 border-slate-700">
+                        <div className=" flex gap-3">
+                            <button onClick={() => addFontStyle("bold")} className={clsx(styelButton, "font-bold")}>B</button>
+                            <button onClick={() => addFontStyle("italic")} className={clsx(styelButton, "italic")} >i</button>
+                            <button onClick={() => addFontStyle("underline")} className={clsx(styelButton, "underline")} >U</button>
+                            <button className={clsx(styelButton, "line-through")} >S</button>
+                        </div>
                     </div>
                     <div className=" px-10 py-3">
-                        <div contentEditable={true} suppressContentEditableWarning={true} ref={TranslateFrame} className="flex min-h-7 whitespace-pre-wrap break-words">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sunt omnis odio laudantium, praesentium quos reiciendis accusantium aliquid. Deleniti porro doloremque voluptatum dicta aliquid molestiae atque beatae aspernatur harum illo.
+                        <div
+                            contentEditable={true}
+                            data-content-editable-node={true}
+                            suppressContentEditableWarning={true}
+                            ref={TranslateFrame}
+                            className=" min-h-7 whitespace-pre-wrap break-words"
+
+                        >
+                            {"Lorem ipsum dolor sit "}<span data-token-index={1}>amet</span>{" consectetur adipisicing elit. Tempore sunt omnis odio laudantium, praesentium quos reiciendis accusantium aliquid. Deleniti porro doloremque voluptatum dicta aliquid molestiae atque beatae aspernatur harum illo."}
                         </div>
 
 
